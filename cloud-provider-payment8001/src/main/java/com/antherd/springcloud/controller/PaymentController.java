de0dcb7276ee6ff1a9ac5493dcb599b996714d53
geyiwei <geyiwei@outlook.com>
2020-12-07 17:50:48 +0800

    主模块及支付模块搭建

---
+++ cloud-provider-payment8001/src/main/java/com/antherd/springcloud/controller/PaymentController.java
@@ -0,0 +1,40 @@
+package com.antherd.springcloud.controller;
+
+import com.antherd.springcloud.entities.CommonResult;
+import com.antherd.springcloud.entities.Payment;
+import com.antherd.springcloud.service.PaymentService;
+import javax.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class PaymentController {
+  @Resource
+  private PaymentService paymentService;
+
+  @PostMapping(value = "/payment")
+  public CommonResult<Payment> create(Payment payment) {
+    int result = paymentService.create(payment);
+    log.info("*****插入结果： " + result);
+    if(result > 0) {
+      return new CommonResult(200, "插入数据库成功", result);
+    } else {
+      return new CommonResult<>(444, "插入数据库失败",null);
+    }
+  }
+
+  @GetMapping(value = "/payment/{id}")
+  public CommonResult getPaymentById(@PathVariable Long id) {
+    Payment payment = paymentService.getPaymentById(id);
+    log.info("*****查询结果： " + payment);
+    if (payment != null) {
+      return new CommonResult(200, "查询成功", payment);
+    } else {
+      return new CommonResult(444, "没有对应记录，查询ID: " + id,null);
+    }
+  }
+}
