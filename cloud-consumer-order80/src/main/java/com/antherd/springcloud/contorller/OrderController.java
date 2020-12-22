7909b779eef03a7180def39453f3cdf54d4f8c35
geyiwei <geyiwei@outlook.com>
2020-12-22 16:51:10 +0800

    添加getForEntity demo

--- cloud-consumer-order80/src/main/java/com/antherd/springcloud/contorller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/antherd/springcloud/contorller/OrderController.java
@@ -4,6 +4,7 @@ import com.antherd.springcloud.entities.CommonResult;
 import com.antherd.springcloud.entities.Payment;
 import javax.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,5 +29,15 @@ public class OrderController {
   @GetMapping("/consumer/payment/{id}")
   public CommonResult<Payment> getPayment(@PathVariable Long id) {
     return restTemplate.getForObject(PAYMENT_URL + "/payment/" + id, CommonResult.class);
+  }
+
+  @GetMapping("/consumer/payment2/{id}")
+  public CommonResult<Payment> getPayment2(@PathVariable Long id) {
+    ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/payment/" + id, CommonResult.class);
+    if (entity.getStatusCode().is2xxSuccessful()) {
+      return entity.getBody();
+    } else {
+      return new CommonResult<>(444, "操作失败");
+    }
   }
 }
