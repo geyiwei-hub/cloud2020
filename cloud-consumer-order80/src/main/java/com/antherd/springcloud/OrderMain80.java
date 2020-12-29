5ae1a4f9eff4d967ef8361b5f1acefdc6130af09
geyiwei <geyiwei@outlook.com>
2020-12-29 16:38:52 +0800

    Ribbon随机负载均衡

--- cloud-consumer-order80/src/main/java/com/antherd/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/antherd/springcloud/OrderMain80.java
@@ -1,11 +1,14 @@
 package com.antherd.springcloud;
 
+import com.antherd.rule.MyRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE", configuration = MyRule.class)
 public class OrderMain80 {
 
   public static void main(String[] args) {
