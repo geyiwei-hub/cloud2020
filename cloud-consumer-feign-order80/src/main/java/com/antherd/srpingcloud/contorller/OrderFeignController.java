2d75e85413b9723245845d3734cac073771605cd
geyiwei <geyiwei@outlook.com>
2020-12-30 14:04:44 +0800

    OpenFeign服务调用

---
+++ cloud-consumer-feign-order80/src/main/java/com/antherd/srpingcloud/contorller/OrderFeignController.java
@@ -0,0 +1,23 @@
+package com.antherd.srpingcloud.contorller;
+
+import com.antherd.springcloud.entities.CommonResult;
+import com.antherd.springcloud.entities.Payment;
+import com.antherd.srpingcloud.service.PaymentFeignService;
+import javax.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class OrderFeignController {
+
+  @Resource
+  private PaymentFeignService paymentFeignService;
+
+  @GetMapping("/consumer/payment/{id}")
+  public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
+    return paymentFeignService.getPaymentById(id);
+  }
+}
