1156a94f6ace7daaa9a00c887e5f0389a267b7f8
geyiwei <geyiwei@outlook.com>
2021-01-15 10:21:29 +0800

    Sentinel热点key

--- cloudalibaba-sentinel-service8401/src/main/java/com/antherd/springcloud/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/antherd/springcloud/alibaba/controller/FlowLimitController.java
@@ -1,6 +1,9 @@
 package com.antherd.springcloud.alibaba.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +17,16 @@ public class FlowLimitController {
   @GetMapping("/testB")
   public String TestB() {
     return "****** testB";
+  }
+
+  @GetMapping("/testHotKey")
+  @SentinelResource(value = "testHotKey", blockHandler = "dealTestHotKey") // value：sentinel热点key资源名
+  public String testHotKey(@RequestParam(value = "p1", required = false) String p1,
+      @RequestParam(value = "p2", required = false) String p2) {
+    return "***** testHotKey";
+  }
+
+  public String dealTestHotKey(String p1, String p2, BlockException exception) {
+    return "***** dealTestHotKey o(╥﹏╥)o"; // sentinel 系统默认的提示：Blocked by Sentinel (flow limiting)
   }
 }
