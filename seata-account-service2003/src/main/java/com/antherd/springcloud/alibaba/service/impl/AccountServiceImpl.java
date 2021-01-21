911de2c8e1cdb1d029bde53a8f8feeef99b0152e
geyiwei <geyiwei@outlook.com>
2021-01-21 10:54:37 +0800

    模拟超时异常

--- seata-account-service2003/src/main/java/com/antherd/springcloud/alibaba/service/impl/AccountServiceImpl.java
+++ seata-account-service2003/src/main/java/com/antherd/springcloud/alibaba/service/impl/AccountServiceImpl.java
@@ -3,6 +3,7 @@ package com.antherd.springcloud.alibaba.service.impl;
 import com.antherd.springcloud.alibaba.dao.AccountDao;
 import com.antherd.springcloud.alibaba.service.AccountService;
 import java.math.BigDecimal;
+import java.util.concurrent.TimeUnit;
 import javax.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -21,6 +22,12 @@ public class AccountServiceImpl implements AccountService {
   public void decrease(Long userId, BigDecimal money) {
 
     log.info("***** account-service中扣减账户余额开始");
+    // 模拟超时异常，全局事务回滚
+    try {
+      TimeUnit.SECONDS.sleep(20);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
     accountDao.decrease(userId, money);
     log.info("***** storage-service中扣减账户余额结束");
   }
